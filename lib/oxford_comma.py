e53ebfa2fbe3449322a56fe0aa707bf884e1aec1
Princeton J. Rose <princetonjrose@gmail.com>
2022-12-28 16:48:16 -0500

    finished lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,13 @@
+import ipdb
+
 def oxford_comma(items):
-    return None
+    # ipdb.set_trace()
+    if len( items ) <= 1:
+        return ''.join( items )
+    elif len( items ) == 2:
+        return ' and '.join( items )
+    else:
+        joined = ', '.join( items[ 0:-1 ] )
+        add_and = ', and ' + items[-1]
+        return joined + add_and
+
