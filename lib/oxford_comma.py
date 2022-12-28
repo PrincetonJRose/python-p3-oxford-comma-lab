cd1f0436b85b5a51692b4638b9ef0b97257abdf6
Princeton J. Rose <princetonjrose@gmail.com>
2022-12-28 16:49:55 -0500

    updated solution

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -2,9 +2,7 @@ import ipdb
 
 def oxford_comma(items):
     # ipdb.set_trace()
-    if len( items ) <= 1:
-        return ''.join( items )
-    elif len( items ) == 2:
+    if len( items ) <= 2:
         return ' and '.join( items )
     else:
         joined = ', '.join( items[ 0:-1 ] )
